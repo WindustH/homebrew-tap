a32c913da8d9d9001e827deef36e86221dd6a981
windusth <windusth2006@gmail.com>
2026-07-01 00:30:00 +0800

    Add HEAD build for gallery-tui

--- Formula/gallery-tui.rb
+++ Formula/gallery-tui.rb
@@ -4,6 +4,7 @@ class GalleryTui < Formula
   url "https://github.com/WindustH/gallery-tui/archive/refs/tags/v0.1.1.tar.gz"
   sha256 "6da590cf3661e29c1f3ea7baf0ebd1c348b681cfdfea6f3f853cb236ee962d25"
   license "MIT"
+  head "https://github.com/WindustH/gallery-tui.git", branch: "master"
 
   depends_on "rust" => :build
   depends_on "chafa"
