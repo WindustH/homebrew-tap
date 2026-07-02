7ac5caddaa3a65ff50a6d98ad8541d129ccf7083
windusth <windusth2006@gmail.com>
2026-07-02 20:41:34 +0800

    Update gallery-tui to 0.1.5

--- Formula/gallery-tui.rb
+++ Formula/gallery-tui.rb
@@ -1,7 +1,7 @@
 class GalleryTui < Formula
   desc "Terminal image gallery powered by Ratatui and Chafa"
   homepage "https://github.com/WindustH/gallery-tui"
-  version "0.1.4"
+  version "0.1.5"
   license "MIT"
 
   head do
@@ -11,15 +11,15 @@ class GalleryTui < Formula
 
   if OS.mac?
     if Hardware::CPU.arm?
-      url "https://github.com/WindustH/gallery-tui/releases/download/v0.1.4/gallery-tui-0.1.4-aarch64-apple-darwin.tar.gz"
-      sha256 "f5503396a8009a00b683394d8b08016e241bf04d71bb38fb6775fe1eae5d06dd"
+      url "https://github.com/WindustH/gallery-tui/releases/download/v0.1.5/gallery-tui-0.1.5-aarch64-apple-darwin.tar.gz"
+      sha256 "a7621c6379aef8c800a8c1c9c089af275f4ff50c3fb25f1d8c28405bd31eec9b"
     else
-      url "https://github.com/WindustH/gallery-tui/releases/download/v0.1.4/gallery-tui-0.1.4-x86_64-apple-darwin.tar.gz"
-      sha256 "8b00b2a544718099aeffb0ee3f30f8c3bf037d4c1be38e87b58347ba312f9e87"
+      url "https://github.com/WindustH/gallery-tui/releases/download/v0.1.5/gallery-tui-0.1.5-x86_64-apple-darwin.tar.gz"
+      sha256 "b5e9e5eaf3fe9e90e3260139b63f29c76d1206cc10cb3813a88dc18e15042979"
     end
   elsif OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/WindustH/gallery-tui/releases/download/v0.1.4/gallery-tui-0.1.4-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "ac75cac03d73238680f1ccf48558a8c29f5d82fe1d2167dd4435e6ab6590544a"
+    url "https://github.com/WindustH/gallery-tui/releases/download/v0.1.5/gallery-tui-0.1.5-x86_64-unknown-linux-gnu.tar.gz"
+    sha256 "4505c658a47512dfb0ec0b7b4801da6d8f66097fdccaa6200cbd0bebc0684b46"
   end
 
   depends_on "rust" => :build if build.head?
