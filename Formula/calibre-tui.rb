fb5391ea37c5a9a011ce7007fc3d3bc9281ce75d
windusth <windusth2006@gmail.com>
2026-07-14 03:03:10 +0800

    Update calibre-tui to 0.5.1

--- Formula/calibre-tui.rb
+++ Formula/calibre-tui.rb
@@ -1,21 +1,21 @@
 class CalibreTui < Formula
   desc "Search and open books in a Calibre library from a terminal UI"
   homepage "https://github.com/WindustH/calibre-tui"
-  version "0.5.0"
+  version "0.5.1"
   license "MIT"
   head "https://github.com/WindustH/calibre-tui.git", branch: "master"
 
   if OS.mac?
     if Hardware::CPU.arm?
-      url "https://github.com/WindustH/calibre-tui/releases/download/v0.5.0/calibre-tui-0.5.0-aarch64-apple-darwin.tar.gz"
-      sha256 "9f2a7b2a4caedf738bebb9f6966b6ed1d72d69f279b1412e7fd68cb0e38d0f62"
+      url "https://github.com/WindustH/calibre-tui/releases/download/v0.5.1/calibre-tui-0.5.1-aarch64-apple-darwin.tar.gz"
+      sha256 "d6f283440abe29b3a19c95a36af20253f84d75ed26c66ee0bfb659f8b99fe59b"
     else
-      url "https://github.com/WindustH/calibre-tui/releases/download/v0.5.0/calibre-tui-0.5.0-x86_64-apple-darwin.tar.gz"
-      sha256 "712df9e8dd155f820949db40791df71f5b2a57a71db97938ecc648e9acffbc8a"
+      url "https://github.com/WindustH/calibre-tui/releases/download/v0.5.1/calibre-tui-0.5.1-x86_64-apple-darwin.tar.gz"
+      sha256 "455ee9f7d80164e27bfcbba35df2b4868566a6de53f0e3ce8663b4c5f9d7fd3a"
     end
   elsif OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/WindustH/calibre-tui/releases/download/v0.5.0/calibre-tui-0.5.0-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "754433e8683202fdb0d4959eaef97a1c769fe1adf6be1a2e91b1e98ac77958b1"
+    url "https://github.com/WindustH/calibre-tui/releases/download/v0.5.1/calibre-tui-0.5.1-x86_64-unknown-linux-gnu.tar.gz"
+    sha256 "880cb6f6593d5da020bc0affac1b25fc968c496914799d0e90097439b8bb74ce"
   end
 
   depends_on "rust" => :build if build.head?
