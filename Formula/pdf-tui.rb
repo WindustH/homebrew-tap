5a2d2256553e6ad46f99b4f480e82e7ee706354f
windusth <windusth2006@gmail.com>
2026-07-16 20:50:15 +0800

    Update pdf-tui to 0.1.1

--- Formula/pdf-tui.rb
+++ Formula/pdf-tui.rb
@@ -1,7 +1,7 @@
 class PdfTui < Formula
   desc "Terminal PDF reader built with Ratatui and terminal graphics protocols"
   homepage "https://github.com/WindustH/pdf-tui"
-  version "0.1.0"
+  version "0.1.1"
   license "MIT"
 
   head do
@@ -10,16 +10,17 @@ class PdfTui < Formula
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/WindustH/pdf-tui/releases/download/v0.1.0/pdf-tui-0.1.0-aarch64-apple-darwin.tar.gz"
-    sha256 "18f0fb348f6c8b9524c50ea4ce1cfccb662eb83ff3ba77b5a59e4c21dfd35ddd"
+    url "https://github.com/WindustH/pdf-tui/releases/download/v0.1.1/pdf-tui-0.1.1-aarch64-apple-darwin.tar.gz"
+    sha256 "2f17468114d65046e5d6b14e4d36eb99bc6a78beff714d351e50e0f5fec39b9d"
   elsif OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/WindustH/pdf-tui/releases/download/v0.1.0/pdf-tui-0.1.0-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "49fceec935b904bb1b38fb86411784405ed766929938f9384aaf74f43ff01dca"
+    url "https://github.com/WindustH/pdf-tui/releases/download/v0.1.1/pdf-tui-0.1.1-x86_64-unknown-linux-gnu.tar.gz"
+    sha256 "fc3597063abe5d1ecdde92062bbd7108ef5c697558a8474391192c111ea17a13"
   end
 
   depends_on "rust" => :build if build.head?
   depends_on "chafa"
   depends_on "exiftool"
+  depends_on "pdftk-java"
   depends_on "poppler"
 
   def install
