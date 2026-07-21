52776a91980639da5c5837fdc648f701698f8aee
windusth <windusth2006@gmail.com>
2026-07-22 02:47:22 +0800

    Update pdf-tui to 0.1.9

--- Formula/pdf-tui.rb
+++ Formula/pdf-tui.rb
@@ -1,8 +1,9 @@
 class PdfTui < Formula
   desc "Terminal PDF reader built with Ratatui and terminal graphics protocols"
   homepage "https://github.com/WindustH/pdf-tui"
-  version "0.1.8"
+  version "0.1.9"
   license "MIT"
+  PDFIUM_RELEASE = "7961"
 
   head do
     url "https://github.com/WindustH/pdf-tui.git", branch: "master"
@@ -10,27 +11,65 @@ class PdfTui < Formula
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/WindustH/pdf-tui/releases/download/v0.1.8/pdf-tui-0.1.8-aarch64-apple-darwin.tar.gz"
-    sha256 "c983f73762b3d6e884e1f0873994447dc19b284a9a6e6a11eb91ffb307a06d59"
+    url "https://github.com/WindustH/pdf-tui/releases/download/v0.1.9/pdf-tui-0.1.9-aarch64-apple-darwin.tar.gz"
+    sha256 "f5fa2319063ef72cdd08919444e4437daa3cd3749138e8d049de60ad02c8e8ba"
   elsif OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/WindustH/pdf-tui/releases/download/v0.1.8/pdf-tui-0.1.8-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "c01c1e2da09869b38c936e6c7d1aa6cd5bf0a1fd72e4781f358e31576b58af63"
+    url "https://github.com/WindustH/pdf-tui/releases/download/v0.1.9/pdf-tui-0.1.9-x86_64-unknown-linux-gnu.tar.gz"
+    sha256 "463d248861b3864aac8f8dea76454e2d180af8195e2750c6a8c79c35cbf49c28"
   end
 
   depends_on "rust" => :build if build.head?
   depends_on "chafa"
   depends_on "exiftool"
+  depends_on "mupdf"
   depends_on "pdftk-java"
   depends_on "poppler"
 
+  if OS.mac?
+    resource "pdfium" do
+      url "https://github.com/bblanchon/pdfium-binaries/releases/download/chromium%2F#{PDFIUM_RELEASE}/pdfium-mac-univ.tgz"
+      sha256 "432ba1831a4581cb0d52550fdad977d0ebf4f31188223b5ddd99b9b89d7124fe"
+    end
+  elsif OS.linux? && Hardware::CPU.intel?
+    resource "pdfium" do
+      url "https://github.com/bblanchon/pdfium-binaries/releases/download/chromium%2F#{PDFIUM_RELEASE}/pdfium-linux-x64.tgz"
+      sha256 "019665c8877d46fe65f625f80fd714ab07aac68554b0636acf2a2adf9288adb2"
+    end
+  end
+
   def install
     if build.head?
+      system "git", "submodule", "update", "--init", "--recursive"
       system Formula["rust"].opt_bin/"cargo", "install", *std_cargo_args
+      libexec.install bin/"pdf-tui" => "pdf-tui.real"
+      rm bin/"pdf-tui"
     else
-      bin.install "pdf-tui"
+      libexec.install "pdf-tui" => "pdf-tui.real"
       doc.install "README.md"
       doc.install "doc"
     end
+    install_pdfium_resource
+    write_pdf_tui_wrapper
+  end
+
+  def install_pdfium_resource
+    resource("pdfium").stage do
+      (libexec/"pdfium").install "lib", "LICENSE", "VERSION", "licenses"
+    end
+  end
+
+  def pdfium_library_path
+    library = OS.mac? ? "libpdfium.dylib" : "libpdfium.so"
+    libexec/"pdfium/lib/#{library}"
+  end
+
+  def write_pdf_tui_wrapper
+    (bin/"pdf-tui").write <<~EOS
+      #!/bin/bash
+      export PDF_TUI_PDFIUM_LIBRARY_PATH="${PDF_TUI_PDFIUM_LIBRARY_PATH:-#{pdfium_library_path}}"
+      exec "#{libexec}/pdf-tui.real" "$@"
+    EOS
+    chmod 0755, bin/"pdf-tui"
   end
 
   test do
