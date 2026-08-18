1c5d95e9da8058c6701ff9a95ea1393156f7265e
windusth <windusth2006@gmail.com>
2026-08-18 17:03:42 +0800

    Update music-tui to 0.1.2

--- Formula/music-tui.rb
+++ Formula/music-tui.rb
@@ -1,16 +1,16 @@
 class MusicTui < Formula
   desc "Terminal music player backed by MPD, with covers, synced lyrics and a visualizer"
   homepage "https://github.com/WindustH/music-tui"
-  version "0.1.1"
+  version "0.1.2"
   license "MIT"
   head "https://github.com/WindustH/music-tui.git", branch: "main"
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/WindustH/music-tui/releases/download/v0.1.1/music-tui-0.1.1-aarch64-apple-darwin.tar.gz"
-    sha256 "efb9dc0e9e6cb01e4803a899f86ed756792012e9f2b6d90c2afb1f5dedb9613b"
+    url "https://github.com/WindustH/music-tui/releases/download/v0.1.2/music-tui-0.1.2-aarch64-apple-darwin.tar.gz"
+    sha256 "0cf8f572c75e9955403bfb1250a1d726d055b1be4794314ef1d5a104a8148f4f"
   elsif OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/WindustH/music-tui/releases/download/v0.1.1/music-tui-0.1.1-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "ac8c18f09ee54e9717632c0c67e99a63e018600c75399e83a165ce7977e1722d"
+    url "https://github.com/WindustH/music-tui/releases/download/v0.1.2/music-tui-0.1.2-x86_64-unknown-linux-gnu.tar.gz"
+    sha256 "a95c039d2607df46c3000d979ad88a1d9c7e02248de45cd4b6781968c5b816cf"
   end
 
   depends_on "rust" => :build if build.head?
