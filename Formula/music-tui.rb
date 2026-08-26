b8c44e010a25b33a456e32bc2a08f639b05e98d7
WindustH <windusth2006@gmail.com>
2026-08-26 22:09:21 +0800

    Fix HEAD build dependency declaration

--- Formula/music-tui.rb
+++ Formula/music-tui.rb
@@ -3,7 +3,10 @@ class MusicTui < Formula
   homepage "https://github.com/WindustH/music-tui"
   version "0.1.4"
   license "MIT"
-  head "https://github.com/WindustH/music-tui.git", branch: "main"
+  head do
+    url "https://github.com/WindustH/music-tui.git", branch: "main"
+    depends_on "rust" => :build
+  end
 
   if OS.mac? && Hardware::CPU.arm?
     url "https://github.com/WindustH/music-tui/releases/download/v0.1.4/music-tui-0.1.4-aarch64-apple-darwin.tar.gz"
@@ -13,7 +16,6 @@ class MusicTui < Formula
     sha256 "2a13e2e97448609925cccab315ed10121c4a4d8d4f799c4a44c26e8f97591372"
   end
 
-  depends_on "rust" => :build if build.head?
   depends_on "mpd"
   depends_on "chafa"
   depends_on "sqlite"
