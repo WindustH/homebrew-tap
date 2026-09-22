699aeccf96bf02b9d76ac2d8e9574fe3e4de23d4
windusth <windusth2006@gmail.com>
2026-09-22 21:49:26 +0800

    Update pdf-tui to 0.2.9

--- Formula/pdf-tui.rb
+++ Formula/pdf-tui.rb
@@ -1,7 +1,7 @@
 class PdfTui < Formula
   desc "Terminal PDF reader built with Ratatui and terminal graphics protocols"
   homepage "https://github.com/WindustH/pdf-tui"
-  version "0.2.8"
+  version "0.2.9"
   license "MIT"
   PDFIUM_RELEASE = "7961"
 
@@ -11,11 +11,11 @@ class PdfTui < Formula
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/WindustH/pdf-tui/releases/download/v0.2.8/pdf-tui-0.2.8-aarch64-apple-darwin.tar.gz"
-    sha256 "46cdc5941b17194fa74cf1806b5a5e8bc6dc391814cb0cd02fe3fb0139c44064"
+    url "https://github.com/WindustH/pdf-tui/releases/download/v0.2.9/pdf-tui-0.2.9-aarch64-apple-darwin.tar.gz"
+    sha256 "386f88cfdec241ab50f1130b68301a70a03675bf883d5f5c54f796b367265924"
   elsif OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/WindustH/pdf-tui/releases/download/v0.2.8/pdf-tui-0.2.8-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "6dde645bc2a4b1091eb7a83de9d6a593ee14c8c375ef269e4f0af6412603e3a7"
+    url "https://github.com/WindustH/pdf-tui/releases/download/v0.2.9/pdf-tui-0.2.9-x86_64-unknown-linux-gnu.tar.gz"
+    sha256 "3b1b89796576524a0ac680a3df17b84d77dcd20c0dba341acb24d8665d8bd0bd"
   end
 
   depends_on "rust" => :build if build.head?
